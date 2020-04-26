c1638201118ef9c644fdf7b90cbcc2ce455a4883
Pratham Gupta <pratham18072@iiitd.ac.in>
2020-04-26 17:22:18 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+#Harmony - A DBMS Project
---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask.ext.mysql import MySQL
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_USER'] = 'pratham'
+app.config['MYSQL_DATBASE_PASSWORD'] = 'Pratham@123'
+app.config['MYSQL_DATABASE_DB'] = 'dbms'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
+
+
+from app import routes
